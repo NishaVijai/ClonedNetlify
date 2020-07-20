e8f5ef0679aff9ebb5847bc2a78361237cfd21b3
Nisha <nishavijai@gmail.com>
2020-07-20 14:12:42 +0200

    ADD TAILWIND CDN

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="bg-black">Test</h1>
+    <div className="App font-sans leading-tight">
+      <h1 className="bg-red-300">Test</h1>
     </div>
   );
 }
